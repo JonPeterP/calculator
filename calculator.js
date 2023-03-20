e17095e9d1b7f2940d7bad4a161606a0eede652f
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-03-20 13:31:36 +0800

    Add basic operate function

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let num1 = 0;
+let num2 = 0;
+let result;
+let operator;
+
+function operate(num1, num2, operator){
+    if(operator == "+") return num1 + num2;
+    if(operator == "-") return num1 - num2;
+    if(operator == "*") return num1 * num2;
+    if(operator == "/") return num1 / num2;
+}
+
+
+console.log(operate(5,6, "/"));
